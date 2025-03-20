4e666ba20a5a548eb62170e06c831daae0c36011
Shivaprasad Bilgundi <shivbbilgundi11@gmail.com>
2025-03-20 13:32:42 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <>
+      <div className="container flex min-h-[100vh] items-center justify-center">
+        <h1 className="text-xl font-medium">Hello App...!</h1>
+      </div>
+    </>
+  );
+}

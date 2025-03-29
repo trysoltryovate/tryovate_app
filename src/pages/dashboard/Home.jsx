6714b754918bf27a9af0b8b689e3b648fe2da7bc
Shivaprasad Bilgundi <shivbbilgundi11@gmail.com>
2025-03-29 19:09:50 +0530

    feat: new employee form added

--- src/pages/dashboard/Home.jsx
+++ src/pages/dashboard/Home.jsx
@@ -1,8 +1,6 @@
 import { useState } from "react";
-import { FaPowerOff } from "react-icons/fa";
-import { Link } from "react-router-dom";
 
-import Logo from "../../assets/logo.png";
+import SecondaryNavbar from "../../components/SecondNavbar";
 import Dashboard from "./Dashboard";
 import Welcome from "./Welcome";
 
@@ -10,26 +8,7 @@ export default function Home() {
   const [showDashboard, setShowDashboard] = useState(true);
   return (
     <>
-      <header className="container h-16 w-full">
-        <nav className="flex h-full w-full items-center justify-between">
-          <Link className="font-bold italic" to={"/"} title="Go to Home page">
-            <img
-              src={Logo}
-              alt="tryovate_logo"
-              title="Tryovate"
-              className="w-[42px] md:w-[52px] lg:w-[62px]"
-            />
-          </Link>
-
-          <Link
-            to={"/login"}
-            className="hover:bg-grey-700 rounded-md bg-gray-200 px-3 py-1 text-gray-500 transition-all duration-300 ease-linear hover:shadow-md md:px-4 md:py-2 md:font-medium"
-            title="Login to Tryovate"
-          >
-            <FaPowerOff />
-          </Link>
-        </nav>
-      </header>
+      <SecondaryNavbar />
 
       <main className="container relative min-h-screen w-full border-2">
         <div className="m-2 flex items-center justify-center gap-x-2">

67ffe70ee5ab6e70f17a5977446ab4d65e670429
Shivaprasad Bilgundi <shivbbilgundi11@gmail.com>
2025-03-31 13:14:25 +0530

    feat: added welcome section in the home page

--- src/pages/dashboard/Home.jsx
+++ src/pages/dashboard/Home.jsx
@@ -11,7 +11,7 @@ export default function Home() {
       <SecondaryNavbar />
 
       <main className="lg-ctr relative min-h-screen w-full">
-        <div className="m-3 mx-auto flex w-max items-center justify-center gap-x-1 rounded-lg border p-1 md:my-4">
+        <div className="absolute left-[50%] right-[50%] m-3 mx-auto flex w-max translate-x-[-120px] items-center justify-center gap-x-1 rounded-lg border p-1 md:my-4">
           <button
             onClick={() => setShowDashboard(true)}
             className={`rounded-md border px-4 py-2 font-medium hover:shadow-md ${showDashboard ? "bg-blue-100 text-blue-800 hover:bg-blue-200" : "bg-gray-100 hover:bg-gray-200"}`}
